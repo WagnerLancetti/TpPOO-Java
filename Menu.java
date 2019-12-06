1c1cdf0083d4faed655dc4cddb0449e252a996b8
WagnerLancetti <56642195+WagnerLancetti@users.noreply.github.com>
2019-12-06 17:19:55 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,22 +1,20 @@
-package locadora;
+package TrabalhoPratico;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+
 import javax.swing.*;
-import java.util.Scanner;
 
 public class Menu extends JFrame implements ActionListener{
-    static Scanner input = new Scanner(System.in);
-    Locadora locadora = new Locadora();
-    JRadioButton sa = new JRadioButton("Sair do Programa",true);
-    JRadioButton lo = new JRadioButton("Entrar no Menu de Locacoes");
-    JRadioButton ca = new JRadioButton("Entrar no Menu de Carros");
-    JLabel titulo = new JLabel("Menu Principal",SwingConstants.CENTER);
-    JLabel titulo2 = new JLabel("Marque o que deseja fazer:",SwingConstants.CENTER);
-    JButton conf = new JButton("Confirmar");
-    ButtonGroup grupo = new ButtonGroup();
+	private static final long serialVersionUID = 1L;
+	boolean menu = true;
     
-    Menu(){
+	
+    public void MenuPrincipal(){
+    	ButtonGroup grupo = new ButtonGroup();
+    	JLabel titulo2 = new JLabel("Marque o que deseja fazer:",SwingConstants.CENTER);
+    	JLabel titulo = new JLabel("Menu Principal",SwingConstants.CENTER);
         setTitle("Menu");
         setSize(500,300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -28,47 +26,56 @@ public class Menu extends JFrame implements ActionListener{
         titulo.setFont(new Font("Arial",Font.BOLD,30));
         add(titulo);
         add(titulo2);
+
+        ArrayList<JRadioButton> botao = new ArrayList<JRadioButton>();
         
-        sa.setHorizontalAlignment(SwingConstants.CENTER);
-        lo.setHorizontalAlignment(SwingConstants.CENTER);
-        ca.setHorizontalAlignment(SwingConstants.CENTER);
-        add(sa);grupo.add(sa);
-        add(lo);grupo.add(lo);
-        add(ca);grupo.add(ca);
+        int i = 0;
         
+        botao.add(new JRadioButton("Sair do Programa",true));
+        botao.get(i).setHorizontalAlignment(SwingConstants.CENTER);
+        add(botao.get(i));
+        grupo.add(botao.get(i));
+        i++;
+        
+        botao.add(new JRadioButton("Entrar no Menu de Locacoes",false));
+        botao.get(i).setHorizontalAlignment(SwingConstants.CENTER);
+        add(botao.get(i));
+        grupo.add(botao.get(i));
+        i++;        
+        
+        botao.add(new JRadioButton("Entrar no Menu de Carros",false));
+        botao.get(i).setHorizontalAlignment(SwingConstants.CENTER);
+        add(botao.get(i));
+        grupo.add(botao.get(i));
+        i++;
+        
+        JButton conf = new JButton("Confirmar");
         conf.setPreferredSize(new Dimension(10,10));
-        conf.addActionListener(this);
+        conf.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e){
+	            if(botao.get(0).isSelected()){    
+	            	System.exit(0);
+	            }    
+	            else if(botao.get(1).isSelected()){    
+	            	MenuLoca m = new MenuLoca();
+	            	m.Menu();
+	            }
+	            else if(botao.get(2).isSelected()){
+//	               MenuCarro m = new MenuCarro();
+//	               m.Menu();
+	            }
+	        }
+    	});
         add(conf);
-    }
-    public void actionPerformed(ActionEvent e){   
-        if(sa.isSelected()){    
-            System.exit(0); 
-        }    
-        else if(lo.isSelected()){    
-            try {
-				mLoca();
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-        }
-        else if(ca.isSelected()){
-            try {
-				mCarros();
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-        }
-    }
-    public void mLoca() throws InterruptedException{
-        MenuLocacao menuLoca = new MenuLocacao();
-        menuLoca.Menu(locadora);
-    }
-    public void mCarros() throws InterruptedException{
-        
-        MenuCarro menuCarros = new MenuCarro();
-        menuCarros.Menu(locadora);
-    }
+    }    
+    
     public static void main (String []args){
-    	new Menu();
+    	Menu m = new Menu();
+    	m.MenuPrincipal();
     }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// To aqui feliz deixa eu queto
+		
+	}
 }
